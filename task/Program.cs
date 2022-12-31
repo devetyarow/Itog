070210c77ce3a690f0d0ca79204533125266e938
Artem <devetyarow@mail.ru>
2022-12-31 14:14:34 +0300

    add markdown

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,26 @@
 ﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых <=3 символам
 
+string[] array1 = new string[5] {"lol", "-34", "iseedeadpeople", "worldclass", "res"};
+string[] array2 = new string[array1.Length];
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(array1, array2);
+PrintArray(array2);
